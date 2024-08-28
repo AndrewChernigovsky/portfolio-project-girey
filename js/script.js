7b7131db70a8e516a0d314d813b441e54890ef2f
Girey <larsvokul@mail.ru>
2024-08-28 11:42:06 +0300

    Сделал active-tab

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 const headWrap = document.querySelector(".header__wrapper");
 const buttonBurger = document.querySelector(".header__burger");
 const headLink = document.querySelectorAll(".header__item-link");
+const travelLink = document.querySelectorAll(".travel__country-item");
 
 
 buttonBurger.addEventListener("click", () => {
@@ -20,3 +21,17 @@ function onLinkClick() {
         link.classList.remove('active-tab');
     })
 };
+
+travelLink.forEach((tlink) => {
+    tlink.addEventListener('click', () => {
+        onTLinkClick();
+        tlink.classList.add('active-tab');
+    });
+});
+
+function onTLinkClick() {
+    travelLink.forEach((tlink) => {
+        tlink.classList.remove('active-tab');
+    })
+};
+
